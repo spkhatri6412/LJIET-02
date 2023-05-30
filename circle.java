de09f6d2c3b2097535b80d96689298355fd1f77a
spkhatri6412 <102245749+spkhatri6412@users.noreply.github.com>
2023-05-30 13:32:47 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,36 @@
+package Basics;
+import java.util.*;
+public class circle {
+
+	double radius,area,circumference;
+	final double PI = 3.14;
+	
+	public void setRadius()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Radius : ");
+		radius = sc.nextDouble();
+	}
+	
+	public void getArea()
+	{
+		area = PI * radius * radius;
+		System.out.println("Area of the circle : "+area);
+	}
+	
+	public void getCircumference()
+	{
+		circumference = 2 * PI * radius;
+		System.out.println("Circumference of the circle : "+circumference);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		circle c1=new circle();
+		c1.setRadius();
+		c1.getArea();
+		c1.getCircumference();
+	}
+
+}
