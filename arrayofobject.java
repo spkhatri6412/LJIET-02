761f69418198068ca97059b00c47988e04eb21e0
spkhatri6412 <102245749+spkhatri6412@users.noreply.github.com>
2023-06-07 11:21:13 +0530

    Add files via upload

---
+++ arrayofobject.java
@@ -0,0 +1,54 @@
+package assigment;
+import java.util.*;
+
+class student
+{
+	int rollNo;
+	float marks;
+	String name;
+	
+	public void setData()
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter the Roll No. : ");
+		this.rollNo = sc.nextInt();
+		System.out.println("Enter the Marks : ");
+		this.marks = sc.nextFloat();
+		System.out.println("Enter the Name : ");
+		this.name = sc.next();
+		System.out.println();
+	}
+	
+	public void getData()
+	{
+		System.out.println("Roll No. : "+rollNo);
+		System.out.println("Marks : "+marks);
+		System.out.println("Name : "+name);
+	}
+}
+
+public class arrayofobject {
+	
+	public static void main (String args[])
+	{
+		student s1[] = new student[5];
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Enter Data of student "+i);
+			s1[i]=new student();
+			s1[i].setData();	
+		}
+		
+		System.out.println("--- Details of the studens ---");
+		System.out.println();
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Details of student "+i);
+			s1[i].getData();
+			System.out.println();
+		}
+		
+	}
+
+}
